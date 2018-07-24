a2018db49a198d7c10e8d8a414ae3970d548d6be
Unknown <mechali.joseph@gmail.com>
2018-07-24 15:43:43 -0400

    forgotten

---
+++ Reprise/Assets/Spells/SpellSystem/SpellOnPaper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellOnPaper {
+
+	SpellAttributes resultingAttributes;
+
+	List<SpellAttributes> attributesModifiers;
+
+	///////////////ecrire les regles de combinaison
+	///////////////traduire le resultat en sort utilisable quand les unités seront mis en place, l ajout de bonus speciaux se fera ici, stockés dans cette classe
+}
